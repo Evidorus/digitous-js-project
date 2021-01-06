1657bef38156711a1c1f1fad8785bbba6966986b
konexio <damir.sagadbekov@live.fr>
2021-01-06 15:49:37 +0100

    v4

--- project.js
+++ project.js
@@ -12,41 +12,67 @@ var grid = [
 ];
 var rover = { 
     direction : 'S',
-    x : 0,
-    y : 0,
+    x : 0, // colonnes
+    y : 0, // lignes
  };
 
-function turnRight([direction]){
-    if (direction === 'N'){
-        return direction = {direction : 'E'}
+function turnRight(rover){
+    if (rover.direction === 'N'){
+        rover.direction = {direction : 'E'}
+        return rover
     } 
-    if (direction === 'E'){
-        return direction = {direction : 'S'}
+    if (rover.direction === 'E'){
+        rover.direction = {direction : 'S'}
+        return rover
     }
-    if (direction === 'S'){
-        return direction = {direction : 'W'}
+    if (rover.direction === 'S'){
+        rover.direction = {direction : 'W'}
+        return rover
     }
-    if (direction === 'W'){
-        return direction = {direction : 'N'}
+    if (rover.direction === 'W'){
+        rover.direction = {direction : 'N'}
+        return rover
     }  
 }
 
-function turnLeft([direction]){
-    if (direction === 'N'){
-        return direction = {direction : 'W'}
+function turnLeft(rover){
+    if (rover.direction === 'N'){
+        rover.direction = {direction : 'W'}
+        return rover
     } 
-    if (direction === 'E'){
-        return direction = {direction : 'N'}
+    if (rover.direction === 'E'){
+        rover.direction = {direction : 'N'}
+        return rover
     }
-    if (direction === 'S'){
-        return direction = {direction : 'E'}
+    if (rover.direction === 'S'){
+        rover.direction = {direction : 'E'}
+        return rover
     }
-    if (direction === 'W'){
-        return direction = {direction : 'S'}
+    if (rover.direction === 'W'){
+        rover.direction = {direction : 'S'}
+        return rover
     }  
 }
 
+function moveForward(rover){
+    if (rover.direction === 'N'){
+        rover.y = rover.y + 1;
+        return rover
+    }
+    if (rover.direction === 'E'){
+        rover.x = rover.x + 1;
+        return rover
+    }
+    if (rover.direction === 'S'){
+        rover.y = rover.y - 1;
+        return rover
+    }
+    if (rover.direction === 'W'){
+        rover.x = rover.x - 1;
+        return rover
+    }
+}
 
-
-console.log(turnRight(rover.direction))
-console.log(turnLeft(rover.direction))
+// console.log(turnRight(rover))
+// console.log(turnLeft(rover))
+console.log(moveForward(rover))
