208a0b98c1be995bec574e6554f2651ffc7c4d61
Mgoumane <goumane.mahmoudou@gmail.com>
2021-01-06 12:26:40 +0100

    modif

--- project.js
+++ project.js
@@ -22,3 +22,10 @@ function turnRight (rover){
 }
 console.log(rover)
 console.log(turnLeft(rover))
+
+function moveForward(rover){
+    changement = { x : 0 , y : 1}
+    return changement
+}
+
+console.log(moveForward(rover));
